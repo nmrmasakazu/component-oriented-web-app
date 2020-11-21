ecafc297022e5b8cba2f53af756f27adad7e8539
nmrmasakazu <m.nomura.733@nitech.jp>
2020-11-21 13:01:24 +0900

    ログイン画面完成

--- app/src/client/layouts/index.tsx
+++ app/src/client/layouts/index.tsx
@@ -6,11 +6,15 @@ export default () => (
     <Head>
       <meta charSet="utf-8" />
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.6.2/css/bulma.min.css" />
+      <link rel="stylesheet" href="/static/bootstrap/css/bootstrap.min.css" />
+      <link rel="stylesheet" href="/static/css/Contact-Form-Clean.css" />
+      <link rel="stylesheet" href="/static/css/styles-index.css" />
+      <link rel="stylesheet" href="/static/css/styles-promisedetail.css" />
     </Head>
     <body>
       <Main />
       <NextScript />
+      <style>{`body {background-color: rgb(0, 157, 158)}`}</style>
     </body>
   </html>
 )
--- app/src/client/pages/login.tsx
+++ app/src/client/pages/login.tsx
@@ -27,43 +27,57 @@ const LoginPage = () => {
   }
 
   return <>
-    <Links />
-    {/* <div class="contact-clean" style="background-color: rgb(0, 157, 158);opacity: 1;">
-        <form id="form-round" method="post" th:action="@{/signin}"><img class="float-right d-md-flex align-items-md-start img-right" th:src="@{~/img/Nitech-logo.png}" style="height: 50px;padding: 5px 0 0 0;" width="auto" height="50px">
-            <div style="height: 100px;padding: 0 0 20px 0;margin: 0 0 10px 0;"><img class="float-right d-md-flex img-right" th:src="@{~/img/Kaikokai-logo.png}" style="height: 50px;padding: 0 0 5px;margin: 5px 0 0 0;" width="auto" height="50px"></div>
-            <h2 class="text-center" style="margin: 0px;">テレリハビリ治療支援システム</h2>
-            <h5 class="text-right" style="margin: 0 0 20px;"></h5>
-            <div class="form-group"><input class="form-control" type="text" name="username" placeholder="ユーザーネーム"></div>
-            <div class="form-group"><input class="form-control" type="password" placeholder="パスワード" name="password">
-                <div th:if="${param.error}"><small class="form-text text-danger">パスワードが正しくありません</small></div>
-            </div>
-            <div class="form-group" id="center"><button class="btn" style="margin: 0;width: 150px;background-color: rgb(0, 157, 158);color: rgb(255,255,255);">ログイン</button></div>
-        </form>
+    {/* <Links /> */}
+    <div className="contact-clean">
+      <form id="form-round" onSubmit={handleSubmit}>
+        <img className="float-right d-md-flex align-items-md-start img-right" src="/static/img/Nitech-logo.png" />
+        <div className="kaikokai">
+          <img className="float-right d-md-flex img-right" src="/static/img/Kaikokai-logo.png" />
+        </div>
+        <h2 className="text-center">テレリハビリ治療支援システム</h2>
+        <h5 className="text-right"></h5>
+        {error ? <p>Error: {error}</p> : null}
+        <div className="form-group">
+            <input className="form-control" type="text" name="username" placeholder="ユーザーネーム" onChange={handleInputChange} />
+        </div>
+        <div className="form-group">
+          <input className="form-control" type="password" placeholder="パスワード" name="password" onChange={handleInputChange} />
+        </div>
+        <div className="form-group" id="center">
+            <button className="btn">ログイン</button>
+        </div>
+      </form>
+
+      <style jsx>{`
+        div.contact-clean {
+        background-color: rgb(0, 157, 158);
+        opacity: 1;
+        }
+        h2 {
+          margin: 0px;
+        }
+        h5 {
+          margin: 0 0 20px;
+        }
+        button {
+          background-color: rgb(0, 157, 158) !important;
+          color: rgb(255, 255, 255);
+          margin: 0;
+          width: 150px;
+        }
+        img {
+          height: 50px;
+          padding: 5px 0 0 0;
+          width: "auto";
+          height: "50px"; 
+        }
+        .kaikokai {
+          height: 100px;
+          padding: 0 0 20px 0;
+          margin: 0 0 10px 0;
+        }
+      `}</style>
     </div>
-    <script src="assets/js/jquery.min.js"></script>
-    <script src="assets/bootstrap/js/bootstrap.min.js"></script> */}
-
-
-
-    {error ? <p>Error: {error}</p> : null}
-    <form className='container mx-auto max-w-sm' onSubmit={handleSubmit}>
-      <div>
-        <label htmlFor='text'>usename</label>
-        <input type='text' id='text' name='username' onChange={handleInputChange} value={inputs.username} placeholder='username' />
-      </div>
-      <div>
-        <label htmlFor='password'>Password</label>
-        <input type='password' id='password' name='password' onChange={handleInputChange} value={inputs.password} placeholder='********' />
-      </div>
-      <button type='submit'>Login</button>
-    </form>
-    
-    <style jsx>{`
-    p {
-      color: red;
-    }
-    `}</style>
   </>
 }
-
 export default LoginPage
