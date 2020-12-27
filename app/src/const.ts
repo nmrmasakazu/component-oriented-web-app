fe730792e39f6576c0269ce81d240c137b0f0ab4
nmrmasakazu <m.nomura.733@nitech.jp>
2020-12-27 13:40:01 +0900

    constの分離

---
+++ app/src/const.ts
@@ -0,0 +1,2 @@
+export const host = process.env.HOST ? process.env.HOST: 'localhost'
+export const port = process.env.PORT ? process.env.PORT: '8080'
--- app/src/server/routes/api/auth.ts
+++ app/src/server/routes/api/auth.ts
@@ -1,11 +1,12 @@
 import Express from 'express'
 import fetch from 'cross-fetch'
+import { host, port } from '../../../const'
 
 const authRouter = Express.Router()
 
 authRouter.post('/login', async (req, res) => {
 
-    const response = await fetch(`http://localhost:8080/users/signin?username=${req.query.username}&password=${req.query.password}`, { method: 'POST' })
+    const response = await fetch(`http://${host}:${port}/users/signin?username=${req.query.username}&password=${req.query.password}`, { method: 'POST' })
     .catch( _ => {
         return res.status(404).send({message: 'APIに接続できません'})
     })
@@ -16,8 +17,6 @@ authRouter.post('/login', async (req, res) => {
     }
 
     const json = await response.json()
-    console.log('lkokokko')
-    console.log(json)
     res.status(200).json({ token: json.token })
 
 })
